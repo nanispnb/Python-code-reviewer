697541f65ca43d1148a59b51211c02f502913613
nanispnb <144880297+nanispnb@users.noreply.github.com>
2025-02-14 19:01:32 +0530

    app.py

--- app.py
+++ app.py
@@ -1,27 +1,19 @@
-pip install streamlit
-pip install google-generativeai
 import streamlit as st
 import google.generativeai as ai
 
-ai.configure(api_key="")
+ai.configure(api_key="AIzaSyB2uYmns8srnhUlgQApQenfEoy8zt1zKro")
 
-sys_prompt = """You are a helpful AI Tutor for Data Science. 
-                Students will ask you doubts related to various topics in data science.
-                You are expected to reply in as much detail as possible. 
-                Make sure to take examples while explaining a concept.
-                In case if a student ask any question outside the data science scope, 
-                politely decline and tell them to ask the question from data science domain only.
-                Always include a helpful statement at the end saying that 
-                'In case if your query is not resolved, feel free to click on this link:
-                innomatics.in to get in touch with our mentor in a 1:1 zoom call"""
+sys_prompt = """You are a helpful Python Code Reviewer. 
+You can resovle, review and give feedbacks on potential bugs along with suggessions for fixes.
+In case if someone ask queries which are not relevant to Python or Python Code, politely tell them to ask relevant queries only."""
 
 gemini_model = ai.GenerativeModel(model_name="models/gemini-2.0-flash-exp", system_instruction=sys_prompt)
 
-st.title("Data Science/AI TUTOR")
+st.title("Code Reviewer")
 
 user_input = st.text_input(label="Enter your query/issue")
 
-btn_click = st.button("Click Me!")
+btn_click = st.button("Check Here")
 
 if btn_click == True:
     response = gemini_model.generate_content(user_input)
